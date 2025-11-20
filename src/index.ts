6b8a43fa495e3f80319f87cea48e718bf9a9daf5
Javier Ruelas <javier.ruelas@igt.com>
2025-11-20 12:56:20 -0800

    index: add rag initialization logic

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const docsDir = "./docs";
-const cacheDir = "./cache/";
+const cacheDir = "./cache";
 
 const store: VectorStore = new VectorStore();
 const loader: DocumentLoader = new DocumentLoader(docsDir, cacheDir);
@@ -18,7 +18,16 @@ const embedder = new Embedder();
 /**
  * Initializes our RAG pipeline.
  */
-async function initialize() {}
+// async function initialize() {
+// 	const documentsToProcess = await loader.loadDocuments();
+// 	documentsToProcess.forEach((doc) => {
+// 		const chunks = await splitter.split(doc);
+// 		chunks.forEach((chunk) => {
+// 			const embedding = await embedder.embed()
+// 			store.add(embedding);
+// 		})
+// 	})
+// }
 
 app.listen(port, () => {
 	console.log(`MCP Server running on port ${port}`);
